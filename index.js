e2050437aafc64e78c175b0ecd0a0654f96266a1
daylightcoderj6 <jananibharathi6@gmail.com>
2024-08-19 21:35:03 +0530

    added jwt

--- index.js
+++ index.js
@@ -1,37 +1,74 @@
-const express = require ('express')
-const cors = require ('cors')
-const bodyParser = require ('body-parser')
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { RegistrationModel } = require("./Schema");
+const { connectDb, mongoose } = require("./db");
+const { handleRegistration, handleLogin } = require("./service");
+const app = express();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 
-const app = express()
-
-const {connectDb , mongoose} = require('./db')
-const { handleRegistration , handleLogin } = require('./service')
-
-app.use(cors())
-app.use(bodyParser.json())
-
+app.use(cors());
+app.use(bodyParser.json());
 connectDb();
 
-app.post('/registration' , (apiReq , apiRes) => {
-   handleRegistration(apiReq , apiRes);
-})
 
-app.get('/login/:username/:password' , (apiReq , apiRes) => {
-    handleLogin(apiReq , apiRes);
- })
-
-app.get('/' , (req , res) => {
-    if(mongoose.connection.readyState === 1){
-        res.send("Server working fine and connected to Database")
-        return;
-    }
-    res.send("Server started and working fine")
-})
+const jwtUserkey = process.env.JWT_USERKEY;
 
 
+app.get("/", (req, res) => {
+  if (mongoose.connection.readyState === 1) {
+    res.send("Server working fine and connected to Database");
+    return;
+  }
+  res.send("Server started and working fine");
+});
 
-app.listen(4000 , () => {
-    console.log("Server started at 4000")
-})
+
+app.post("/registration", (apiReq, apiRes) => {
+  handleRegistration(apiReq, apiRes);
+});
+
+
+app.get("/login/:username/:password", (apiReq, apiRes) => {
+  handleLogin(apiReq, apiRes);
+});
+
+
+
+//  authorization middleware
+
+// const verifyUser = async (username) => {
+//   const dbResponse = await RegistrationModel.findOne({ username });
+//   if (dbResponse._id) {
+//     return true;
+//   }
+//   return false;
+// };
+
+// const auth = async (req, res, next) => {
+//   if (req.headers.auth) {
+//     const userToken = req.headers.auth;
+//     try {
+//       const tokenDecoded = jwt.verify(userToken, jwtUserkey);
+//       const username = tokenDecoded.data;
+//       const response = await verifyUser(username);
+//       if (response) {
+//         next();
+//       } else {
+//         res.send(400);
+//       }
+//     } catch (error) {
+//       res.status(400).send("Invalid token");
+//     }
+//   } else {
+//     res.status(400).send("API error");
+//     return;
+//   }
+// };
+
+
+app.listen(4000, () => {
+  console.log("Server started at 4000");
+});
--- service.js
+++ service.js
@@ -1,37 +1,58 @@
 const { RegistrationModel } = require('./Schema')
 const bcrypt = require ('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const jwtUserkey = process.env.JWT_USERKEY ;
 
 
+// registration
 const handleRegistration = async (apiReq , apiRes) => {
 
-      const {username , password} = apiReq.body
-      if(username?.length && password?.length){
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const dbResponse = await RegistrationModel.create({
-           username : username , 
-           password : hashedPassword
-        });
-        if(dbResponse){
-            apiRes.send(dbResponse)
-            return;
+     try {
+        const {username , password} = apiReq.body
+        if(username && password){
+          const hashedPassword = await bcrypt.hash(password, 0)
+          const dbResponse = await RegistrationModel.create({
+             username : username , 
+             password : hashedPassword
+          });
+            apiRes.json({message : "Data added"})
         }
-      }
-     apiRes.send("In correct Data") 
+     } 
+     catch (error){
+        console.error("Error during registration:" , error);
+        apiRes.status(500).send("Internal server error")
+     }
+    
 }
 
+
+// login
 const handleLogin = async (apiReq , apiRes) => {
 
-    const { username , password } = apiReq.params
+   try {
+    const username = apiReq.params.username
+    const password = apiReq.params.password
+
     const dbResponse = await RegistrationModel.findOne({
-        username : username ,
-        password : password ,
+        username : username 
     })  
-    if(dbResponse?._id){
-        console.log(dbResponse)
-        apiRes.send(dbResponse.username);
-        return;
+
+    if(dbResponse?.username){
+        const passwordMatch = await bcrypt.compare(password , dbResponse.password)
+        if(passwordMatch){
+            const token = jwt.sign({ data : username } , jwtUserkey)
+            apiRes.json({username : dbResponse.username , token : token})
+            return;
+        }
     }
+
     apiRes.send("Login Failed")
+   }
+   catch (error){
+    console.error("Error during login")
+    apiRes.send("Login failed")
+}
 }
 
 
